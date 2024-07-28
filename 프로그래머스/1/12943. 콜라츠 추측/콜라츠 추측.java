012aa3f36e79ad89234f554c3341d0088de868ed
areum0116 <61903747+areum0116@users.noreply.github.com>
2024-07-28 11:24:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(long num) {
+        for(int i = 0; i < 500; i++) {
+            if(num == 1) return i;
+            else if(num % 2 == 0) num /= 2;
+            else num = num * 3 + 1;
+        }
+        return -1;
+    }
+}
