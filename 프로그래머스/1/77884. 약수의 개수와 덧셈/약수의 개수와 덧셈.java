3f73e0ea074702a3822ff8ba9f633a3cf17ff1a6
areum0116 <61903747+areum0116@users.noreply.github.com>
2024-07-29 09:15:16 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 5.21 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for(int i = left; i <= right; i++) {
+            int yaksu = 0;
+            for(int j = 1; j <= i; j++) {
+                if(i % j == 0) yaksu++;
+            }
+            answer += yaksu % 2 == 0 ? i : -i;
+        }
+        return answer;
+    }
+}
