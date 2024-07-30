519832a0f8667a44575c3b4f48374371c2cdacc9
areum0116 <61903747+areum0116@users.noreply.github.com>
2024-07-30 09:58:42 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.11 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        int idx = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                idx = -1;
+                sb.append(' ');
+            }
+            else if(idx % 2 == 0) {
+                sb.append(Character.toUpperCase(s.charAt(i)));
+            }
+            else {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+            idx++;
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
