43999b946b55a838eaeab349b6d3ea68e5dc3f01
Areum Kim <areum0116@naver.com>
2025-01-25 17:33:57 +0900

    [level 1] Title: 폰켓몬, Time: 6.01 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+            if(set.size() == nums.length / 2) return set.size();
+        }
+        return set.size();
+    }
+}
