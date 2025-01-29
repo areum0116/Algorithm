47812b75cc8762fbf8de62eca2a4233d91f0c3a4
Areum Kim <areum0116@naver.com>
2025-01-29 21:27:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 56.43 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+
+        Stack<Integer> stack = new Stack<>();
+        for (int num : arr) {
+            if(!stack.isEmpty() && stack.peek() == num) continue;
+            stack.push(num);
+        }
+        answer = new int[stack.size()];
+        for (int i = answer.length-1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}
