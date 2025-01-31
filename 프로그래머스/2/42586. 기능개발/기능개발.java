d4d9ea8de57f794a6bef297ee0d5999a20d8e456
Areum Kim <areum0116@naver.com>
2025-01-31 16:51:30 +0900

    [level 2] Title: 기능개발, Time: 21.61 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,30 +5,22 @@ class Solution {
         int[] answer = {};
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < progresses.length; i++) {
-            int leftPercentage = 100 - progresses[i];
-            int leftDays = (leftPercentage / speeds[i]) + (leftPercentage % speeds[i] == 0 ? 0 : 1);
-            queue.add(leftDays);
+            int leftDates = (100 - progresses[i]) / speeds[i] + ((100 - progresses[i]) % speeds[i] != 0 ? 1 : 0);
+            queue.add(leftDates);
         }
-
-        ArrayList<Integer> list = new ArrayList<>();
-        int sum = 1;
-        int value = queue.poll();
+        int totalDates = 0;
+        ArrayList<Integer> listAnswer = new ArrayList<>();
         while (!queue.isEmpty()) {
-            int value2 = queue.poll();
-            if(value >= value2) {
-                sum++;
+            Integer cur = queue.poll();
+            if (totalDates >= cur) {
+                int index = listAnswer.size() - 1;
+                listAnswer.set(index, listAnswer.get(index) + 1);
             } else {
-                list.add(sum);
-                value = value2;
-                sum = 1;
+                listAnswer.add(1);
+                totalDates += cur - totalDates;
             }
         }
-        list.add(sum);
-
-        answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        answer = listAnswer.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
