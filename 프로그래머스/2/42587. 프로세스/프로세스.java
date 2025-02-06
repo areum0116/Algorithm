f9bdce4fe81103e3533d2635f80705f2f2990e77
Areum Kim <areum0116@naver.com>
2025-02-06 10:37:31 +0900

    [level 2] Title: 프로세스, Time: 0.97 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,28 +2,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        PriorityQueue<Integer> sortedQueue = new PriorityQueue<>(Collections.reverseOrder());
+
         Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < priorities.length; i++) {
-            sortedQueue.add(priorities[i]);
-            queue.add(new int[] {i, priorities[i]});
+            queue.add(new int[]{i, priorities[i]});
         }
 
-        HashMap<Integer, Integer> answerMap = new HashMap<>();
-        int count = 1;
+        Arrays.sort(priorities);
+        int idx = priorities.length - 1;
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
-            if(sortedQueue.peek() == poll[1]) {
-                answerMap.put(poll[0], count++);
-                sortedQueue.poll();
+            if(poll[1] == priorities[idx]) {
+                idx--;
+                answer++;
+                if(poll[0] == location) break;
             } else {
                 queue.add(poll);
             }
         }
-        for (Map.Entry<Integer, Integer> entry : answerMap.entrySet()) {
-            System.out.println("key = " + entry.getKey() + ", value = " + entry.getValue());
-        }
-        answer = answerMap.get(location);
         return answer;
     }
 }
