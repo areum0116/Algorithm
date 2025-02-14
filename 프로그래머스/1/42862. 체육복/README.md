517241152dd1673ee6914baa4be637a5919879aa
Areum Kim <areum0116@naver.com>
2025-02-14 17:43:21 +0900

    [level 1] Title: 체육복, Time: 0.53 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
 
 ### 성능 요약
 
-메모리: 77.9 MB, 시간: 0.04 ms
+메모리: 80.5 MB, 시간: 0.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 14일 09:41:11
+2025년 02월 14일 17:43:18
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,25 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        int[] students = new int[n + 1];
-        Arrays.fill(students, 1);
-        for (int l : lost)
-            students[l]--;
-        for (int r : reserve)
-            students[r]++;
-
-        for (int i = 1; i <= n; i++) {
-            if(students[i] == 1) answer++;
-            else if(students[i] == 0) {
-                if(students[i-1] == 2) {
-                    answer++;
-                } else if(i != n && students[i+1] == 2) {
-                    students[i+1]--;
-                    answer++;
-                }
+        int answer = n - lost.length;
+        boolean[] hasReverse = new boolean[n + 1];
+        for (int r : reserve) {
+            hasReverse[r] = true;
+        }
+        Arrays.sort(lost);
+        for (int i = 0; i < lost.length; i++) {
+            if (hasReverse[lost[i]]) {
+                hasReverse[lost[i]] = false;
+                lost[i] = -1;
+                answer++;
             }
-            else answer++;
+        }
+        for (int l : lost) {
+            if(l == -1) {
+                continue;
+            }
+            if(l > 0 && hasReverse[l-1]) {
+                hasReverse[l-1] = false;
+                answer++;
+            } else if(l < n && hasReverse[l+1]) {
+                hasReverse[l+1] = false;
+                answer++;
+            }
         }
         return answer;
     }
