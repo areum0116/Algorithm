8cb9b405b837054961f34482763ab86df3fe3c96
Areum Kim <areum0116@naver.com>
2025-03-13 12:23:13 +0900

    [Silver III] Title: 안테나, Time: 1192 ms, Memory: 181640 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] houses = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            houses[i] = sc.nextInt();
+        }
+
+        Arrays.sort(houses);
+
+        System.out.println(houses[(N - 1) / 2]);
+    }
+}
